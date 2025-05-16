948e5b9664427d20ec839a4bb6426f5ebaa451f7
vishwas upadhyaya <vishwasu2001@gmail.com>
2025-05-16 22:10:34 +0530

    4th commit

--- healthcare_backend/healthcare_backend/urls.py
+++ healthcare_backend/healthcare_backend/urls.py
@@ -12,5 +12,5 @@ def health_check(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('health/', health_check, name='health_check'),
+    # path('health/', health_check, name='health_check'),
 ]
